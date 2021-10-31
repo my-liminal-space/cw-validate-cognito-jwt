5948d72167197df4a281b416910de7913b06277b
Andy Redhead <andy@far2gone.com>
2021-10-31 21:05:46 +0000

    tidy up

--- run-test.sh
+++ run-test.sh
@@ -3,12 +3,16 @@
 # Expects to find environment variables for valid cognito username and password:
 #   MLS_DDDD_COGNITO_USERNAME
 #   MLS_DDDD_COGNITO_PASSWORD
+#
+# and
+#
+#   MLS_DDDD_APP_CLIENT_ID
 
 set -e
 
 wrangler publish
 
-MLS_VALID_AWS_JWT="$( aws cognito-idp initiate-auth --client-id 5peen44ratf8dm4hpljvtlhd1m --auth-flow USER_PASSWORD_AUTH --auth-parameters USERNAME=${MLS_DDDD_COGNITO_USERNAME},PASSWORD=${MLS_DDDD_COGNITO_PASSWORD} --region us-east-1 | jq -r .AuthenticationResult.IdToken )"
+MLS_VALID_AWS_JWT="$( aws cognito-idp initiate-auth --client-id ${MLS_DDDD_APP_CLIENT_ID} --auth-flow USER_PASSWORD_AUTH --auth-parameters USERNAME=${MLS_DDDD_COGNITO_USERNAME},PASSWORD=${MLS_DDDD_COGNITO_PASSWORD} --region us-east-1 | jq -r .AuthenticationResult.IdToken )"
 
 echo
 echo "${MLS_VALID_AWS_JWT}"
